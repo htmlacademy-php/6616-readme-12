39a3f08e15f4e30b955d7e66cc32f02640712f3a
Aleksei Zubarev <alexeyzuby@bk.ru>
2021-10-05 15:11:19 +0300

    Added filters + post page

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-    const DBHOST = 'localhost';
-    const DBUSER = 'root';
-    const DBPASSWORD = 'root';
-    const DBNAME = 'readme';
+    const DBHOST = '';
+    const DBUSER = '';
+    const DBPASSWORD = '';
+    const DBNAME = '';
